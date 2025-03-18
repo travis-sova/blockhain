b0d6f7605fe5c4909ae5c9a55c317157fecc4f92
travis-sova <travis.sova@tptlive.ee>
2025-03-18 13:30:36 +0200

    sec

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,7 +1,14 @@
 pragma solidity >=0.8.2 <0.9.0;
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+error InvalidSender(address sender);
+error InvalidReceiver(address receiver);
+error InvalidOwner(address owner);
+error InvalidSpender(address spender);
+error InsufficientBalance(address _spender, uint256 balance, uint256 needed);
+error InsufficentAllowances(address _owner, address _spender, uint256 allowanceBalance, uint256 needed);
 
 contract MyToken is IERC20 {
 
@@ -12,6 +19,8 @@ contract MyToken is IERC20 {
 
     mapping(address account => uint256) private _balances;  
     mapping(address account => mapping(address spender => uint256)) private _allowances;
+
+   
 
     constructor (string memory name_, string memory symbol_, uint totalSupply_) {
         _name = name_;
@@ -42,72 +51,110 @@ contract MyToken is IERC20 {
     }
 
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
-        bool success = true;
+       bool success = true;
 
-        if (_from != msg.sender) {
-          success = updateAllowance(_from, _to, _value); }
-
-        if (success) {
-        return updateBalances(_from, _to, _value);
+       if (_from != msg.sender) {
+            success = updateAllowance(_from, _to, _value);              
         }
 
-        return false;
+       if (success) {
+        return updateBalances(_from, _to, _value);
+       }
+
+       emit Transfer(_from, _to, _value);
+
+       return false;
+    }
+
+    function _transfer(address _from, address _to, uint256 _value) internal {
+        if (_from == address(0)) {
+            revert InvalidSender(_from);            
+        }
+
+        if (_to == address(0)) {
+            revert InvalidReceiver(_to);        
+        }
+
+        _update(_from, _to, _value);
+    }
+
+    function _update(address _from, address _to, uint256 _value) internal {
+        if (_balances[_from] < _value) {
+            revert InsufficientBalance(_from, _balances[_from], _value);
+        }
+
+        _balances[_from] = _balances[_from] - _value;
+        _balances[_to] = _balances[_to] + _value;  
+
+        emit Transfer(_from, _to, _value);
     }
 
     function updateAllowance(address _from, address _to, uint256 _value) private returns (bool success) {
         if (_from == address(0)) {
-            return false;
+            revert InvalidSender(_from);            
         }
 
         if (_to == address(0)) {
-            return false;
-        }
+            revert InvalidReceiver(_to);        }   
 
         if (_allowances[_from][_to] < _value) {
             return false;    
-        }
+        } 
 
-        _allowances[_from][_to] = _allowances[_from][_to] - _value;
+         _allowances[_from][_to] -= _value;   
 
-        return true;
+         return true;
     }
-    
-    function updateBalances(address _from, address _to, uint256 _value) private returns (bool success) {
+
+    function updateBalances(address _from, address _to, uint256 _value) private returns (bool success) {        
         if (_from == address(0)) {
-            return false;
+            revert InvalidSender(_from);
         }
 
         if (_to == address(0)) {
-            return false;
-        }
+            revert InvalidReceiver(_to);
+        }   
 
         if (_balances[_from] < _value) {
-            return false;
+            revert InsufficientBalance(_from, _balances[_from], _value);
         }
 
         _balances[_from] = _balances[_from] - _value;
-        _balances[_to] = _balances[_to] + _value;
+        _balances[_to] = _balances[_to] + _value;  
 
         return true;
     }
 
     function transfer(address _to, uint256 _value) public returns (bool success) {
-        return transferFrom(msg.sender, _to, _value);
+       _transfer(msg.sender, _to, _value);
+
+       return true;
     }
+
+
 
     function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
         return _allowances[_owner][_spender];
     }
 
     function approve(address _spender, uint256 _value) public returns (bool success) {
-        if (_spender == address(0)){
-            return false;
-        }
-
-        _allowances[msg.sender][_spender] = _value;
+        _approve(msg.sender, _spender, _value);
 
         return true;
     }
 
+    function _approve(address _owner, address _spender, uint _value) internal {
+        if (_spender == address(0)) {
+            revert InvalidSpender(_spender);
+        }
 
+        if (_owner == address(0)) {
+            revert InvalidOwner(_owner);
+        }
+
+         _allowances[msg.sender][_spender] = _value;
+
+         emit Approval(_owner, _spender, _value);
+    }
 }
+
